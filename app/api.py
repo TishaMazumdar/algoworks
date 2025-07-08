e10355c086eef19e777895f22fc605e65763417d
Tisha Mazumdar <tishamazumdar2004@gmail.com>
2025-07-08 11:42:45 +0530

    Adding a simple UI

--- app/api.py
+++ app/api.py
@@ -1,28 +1,29 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from src.rag.qa_engine import create_qa_chain, query_rag
 from src.rag.retriever import get_vectorstore_retriever
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-# Build chain once (on app startup)
 retriever = get_vectorstore_retriever()
 qa_chain = create_qa_chain(retriever)
 
-class QueryRequest(BaseModel):
-    question: str
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/ask")
-def ask_question(request: QueryRequest):
-    result = query_rag(qa_chain, request.question)
-
-    # Extract sources
+@app.post("/ask-ui", response_class=HTMLResponse)
+def ask_ui(request: Request, question: str = Form(...)):
+    result = query_rag(qa_chain, question)
     sources = list({
         doc.metadata.get("source", "unknown")
         for doc in result["source_documents"]
     })
 
-    return {
+    return templates.TemplateResponse("index.html", {
+        "request": request,
         "answer": result["result"],
         "sources": sources
-    }
+    })
